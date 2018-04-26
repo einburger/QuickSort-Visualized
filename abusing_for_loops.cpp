4af24175facea3f9fbf72777190cf16c9b3e5d9f
Chris Carter <ctobiasc@gmail.com>
2018-04-26 13:35:21 -0500

    Update abusing_for_loops.cpp

--- abusing_for_loops.cpp
+++ abusing_for_loops.cpp
@@ -7,7 +7,7 @@
 int main() 
 {
     /* 1. */ for (int x{5}, y{8}, count{}, xy_xor{x^y}; xy_xor; count += 1 & xy_xor, xy_xor >>= 1, printf("%s", xy_xor ? "" : std::to_string(count).c_str()));
-    /* 2. */ for (int i{1}, N{100}; i < N;  printf("%d: %s%s\n", i, i % 3 ? "" : "fizz", i % 5 ? "" : "buzz"), i++);
+    /* 2. */ for (int i{1}, N{100}; i < N;  printf("%d: %s%s\n", i, i % 3 ? "" : "fizz", i % 5 ? "" : "buzz"), ++i);
     /* 3. */ for (size_t a{}, b{1}, i{}, N{10}; i < N; std::cout << a << ' ', a = b - a, b = a + b, ++i);
     return 0;
 }
